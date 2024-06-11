4f11f42409d483effa3031932907c8f5d5c2a94e
mohamed <maxamedcabdifitaax33@gmail.com>
2024-06-11 10:33:31 +0300

    Resolved merge conflicts

---
+++ Javascript/Easy/2.js
@@ -0,0 +1,17 @@
+//Easy
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+  function createCounter (n) {
+    let current = n;
+    return function() {
+        return current++;
+    }
+}
+
+const counter = createCounter(-2);
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());
+console.log(counter());

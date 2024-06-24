0f455165789550e183b72e4a8af708785764b870
mohamed <maxamedcabdifitaax33@gmail.com>
2024-06-24 17:13:13 +0300

    solved 8

---
+++ Javascript/Easy/8.js
@@ -0,0 +1,24 @@
+function compose(functions) {
+    return function(x) {
+        return functions.reduceRight((acc, fn) => fn(acc), x);
+    };
+}
+
+// Example usage:
+const functions = [
+    x => x + 1,//3
+    x => x * x,//4
+    x => 2 * x //4
+];
+const x = 2;
+
+const composedFunction = compose(functions);
+const result = composedFunction(x);
+console.log(result); // Output: 65
+
+
+
+const fn1 = x => x + 1;
+const fn2 = x => 2 * x;
+
+console.log(fn1(fn2(4)))
